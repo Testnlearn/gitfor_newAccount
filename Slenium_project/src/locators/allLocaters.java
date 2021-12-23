0f2e389b5c849e6720a3e4e93f8d94d714959a4b
Testnlearn <testkatare@gmail.com>
2021-12-23 12:57:52 +0530

    first commit

---
+++ Slenium_project/src/locators/allLocaters.java
@@ -0,0 +1,11 @@
+package locators;
+
+public class allLocaters {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("this is first");
+
+	}
+
+}
