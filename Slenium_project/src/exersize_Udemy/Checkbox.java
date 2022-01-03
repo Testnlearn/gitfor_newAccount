1fe6ede07101700a13c768a99427529ed433a8ff
Testnlearn <testkatare@gmail.com>
2022-01-03 22:36:10 +0530

    checkboxcount

---
+++ Slenium_project/src/exersize_Udemy/Checkbox.java
@@ -0,0 +1,24 @@
+
+package exersize_Udemy;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Checkbox {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.setProperty("webdriver.chrome.driver", "/home/codilar/seleniumSetup/chromedriver");
+		WebDriver driver = new ChromeDriver();
+		driver.get("https://rahulshettyacademy.com/AutomationPractice/");
+		driver.manage().window().maximize();
+		/*driver.findElement(By.id("checkBoxOption1")).click();   //checkbox selected 
+		System.out.println(driver.findElement(By.id("checkBoxOption1")).isSelected()); //verified if seleceted
+		driver.findElement(By.id("checkBoxOption1")).click();                            //deselected checkbox
+		System.out.println(driver.findElement(By.id("checkBoxOption1")).isSelected());  //verified if desleceted  */
+
+		System.out.println(driver.findElements(By.cssSelector("input[type='checkbox']")).size()); //getting count of checkboxes here
+
+	}
+}
